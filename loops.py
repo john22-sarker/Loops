dffcc1cca9308945db1480be5290b7694519a814
john22-sarker <johnsarker790@gmail.com>
2024-07-13 01:13:14 +0600

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,17 @@
-# i = 1
-# while i<= 10:
-#     print(f'5*{i} = {i*5}')
-#     i+= 1
+i = 1
+while i<= 10:
+     print(f'5*{i} = {i*5}')
+     i+= 1
 
 
-# i = 1
-# x = 0
-# while i<=10:
-#     print(x)
-#     x = i+x
-#     # print(x)
-#     i+=1
-#
-# print(f'The T Number is:- {x}')
+i = 1
+x = 0
+while i<=10:
+      print(x)
+      x = i+x
+      print(x)
+     i+=1
+print(f'The T Number is:- {x}')
 
 # Clculator
 firstNumber = float(input('Enter the first number:-'))
